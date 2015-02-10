4ccf5fabaebfa6088f75f24723af1c6bff877483
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-10 14:20:02 -0600

    Added default user.

--- env-template.php
+++ env-template.php
@@ -4,5 +4,13 @@ return array(
 'DB_HOST' => '',
 'DB_NAME' => '',
 'DB_USER' => '',
-'DB_PASS' => ''
+'DB_PASS' => '',
+'DEFAULT_USER_USERNAME' => '',
+'DEFAULT_USER_EMAIL'    => '',
+'DEFAULT_USER_PASSWORD' => '',
+'DEFAULT_USER_STREET'   => '',
+'DEFAULT_USER_APT'      => '',
+'DEFAULT_USER_CITY'     => '',
+'DEFAULT_USER_STATE'     => '',
+'DEFAULT_USER_ZIP'      => ''
 );
