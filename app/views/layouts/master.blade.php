cc79d46aca624f90e5b454364fe3c29689a56084
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-10 17:15:36 -0600

    Created master layout.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+</head>
+<body>
+
+<div class="container">
+	<div class="col-md-8">
+		@yield('content')
+	</div>
+</div>
+	
+</body>
+</html>
