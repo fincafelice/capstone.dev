ebd70d6a68d6ab95fbff192e857f4142d267e7a5
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-12 11:58:03 -0600

    Fixed spacing issue in Users seeder and tags table.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -4,18 +4,18 @@ class UsersTableSeeder extends Seeder {
 
 	public function run()
 	{
-		DB::table('users')->delete();
+	   DB::table('users')->delete();
 
-        $user = new User();
-        $user->username = $_ENV['DEFAULT_USER_USERNAME'];
-        $user->email    = $_ENV['DEFAULT_USER_EMAIL'];
-        $user->password = $_ENV['DEFAULT_USER_PASSWORD'];
-        $user->street   = $_ENV['DEFAULT_USER_STREET'];
-        $user->apt      = $_ENV['DEFAULT_USER_APT'];
-        $user->city     = $_ENV['DEFAULT_USER_CITY'];
-        $user->state    = $_ENV['DEFAULT_USER_STATE'];
-        $user->zip      = $_ENV['DEFAULT_USER_ZIP'];
-   
-        $user->save();
+            $user = new User();
+            $user->username = $_ENV['DEFAULT_USER_USERNAME'];
+            $user->email    = $_ENV['DEFAULT_USER_EMAIL'];
+            $user->password = $_ENV['DEFAULT_USER_PASSWORD'];
+            $user->street   = $_ENV['DEFAULT_USER_STREET'];
+            $user->apt      = $_ENV['DEFAULT_USER_APT'];
+            $user->city     = $_ENV['DEFAULT_USER_CITY'];
+            $user->state    = $_ENV['DEFAULT_USER_STATE'];
+            $user->zip      = $_ENV['DEFAULT_USER_ZIP'];
+       
+            $user->save();
 	}
 }
