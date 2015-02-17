087fe29bca437b94994d0939c98d697b95b23ed1
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-17 11:29:03 -0600

    Added address data to sales table for Google API compatibility.

--- app/database/migrations/2015_02_10_155218_create_sales_table.php
+++ app/database/migrations/2015_02_10_155218_create_sales_table.php
@@ -16,12 +16,14 @@ class CreateSalesTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('sale_name', 255);
+			$table->dateTime('sale_date_time');
+			$table->string('street_num', 255);
 			$table->string('street', 255);
-			$table->string('apt', 20)->nullable();
 			$table->string('city', 255);
 			$table->string('state', 2);
 			$table->string('zip', 5);
-			$table->dateTime('sale_date_time');
+			$table->string('country', 255);
+			$table->string('address', 500);
 			$table->text('description');
 
 			$table->integer('user_id')->unsigned()->default(1);
