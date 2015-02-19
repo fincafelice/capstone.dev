c7d0fe508a1b2ef5543cad4466bfd1284ed2ac1c
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-19 09:33:47 -0600

    Added latitude and longitude to save method.

--- app/controllers/SalesController.php
+++ app/controllers/SalesController.php
@@ -139,7 +139,7 @@ class SalesController extends \BaseController
 
 	protected function saveSale($sale)
 	{
-		// dd(Input::all());
+		dd(Input::all());
 
 		$validator = Validator::make(Input::all(), Sale::$rules);
 
