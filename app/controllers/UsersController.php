739945fd1d1bcca9201687b574d42934d6cd9d67
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-20 10:01:23 -0600

    Troubleshooting create user to redirect to create sale

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -43,7 +43,7 @@ class UsersController extends \BaseController {
 		} else {
 			User::create($data);
 			Session::flash('successMessage', 'New user was created!');
-			return Redirect::route('sales.index');
+			return Redirect::route('sales.create');
 		}
 	}
 
